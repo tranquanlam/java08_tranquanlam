172f59ca5641c29de4d76a8d5b07c04707cf5a12
Your Name <quanlamtran@gmail.com>
2020-01-07 21:20:24 +0700

    CODE Lesson01 07/01/2020

---
+++ lesson01-setup-environment/src/main/java/j4t/lesson01_setup_environment/App.java
@@ -0,0 +1,11 @@
+package j4t.lesson01_setup_environment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
