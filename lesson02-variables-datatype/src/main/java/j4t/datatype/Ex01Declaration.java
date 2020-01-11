c1c7a0ff680795acea68e1a2c3f28e9d8c802f8e
Your Name <quanlamtran@gmail.com>
2020-01-11 14:39:17 +0700

    CODE Lesson02 11/1/2020

---
+++ lesson02-variables-datatype/src/main/java/j4t/datatype/Ex01Declaration.java
@@ -0,0 +1,13 @@
+package j4t.datatype;
+
+public class Ex01Declaration {
+	public static void main(String[] args) {
+		char c = '\u0024';
+		int i = 10;
+		float f = 20.4f;
+		boolean b = true;
+		System.out.println(c);
+		System.out.println(b);
+		//cach cap phat vung nho (kieu du lieu) java
+	}
+}
